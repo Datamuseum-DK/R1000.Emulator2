e1253ff5b6eda2c2a8ff8be1af188ece6239c091
Poul-Henning Kamp <phk@FreeBSD.org>
2023-05-16 18:06:32 +0000

    Add script to run FRU phase 3 for SEQ

---
+++ Emulator/Tests/fp3seq.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Run FRU phase 3 for SEQ
+
+. Tests/subr_test.rc
+
+sc_boards ioc fiu mem0 seq typ val
+
+cli 'sc trace DI*PROC 4'
+cli 'sc quota add 2000'
+cli 'sc quota exit'
+
+# 3 => Execute diagnostics
+# 3 => Run all tests applicable to a given FRU (board)
+# 4 => SEQuencer
+# 3 => Please enter maximum test phase (1-3)
+fru_prompt 3 3 4 3
+
+run
