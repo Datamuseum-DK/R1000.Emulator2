59fbe46c92766e18c3c92c015561ab7da3aa68fc
Poul-Henning Kamp <phk@FreeBSD.org>
2024-01-05 15:13:42 +0000

    Polish XMTRC/MEM32

--- Emulator/NetList/model_xmtrc.py
+++ Emulator/NetList/model_xmtrc.py
@@ -43,29 +43,29 @@ class XMTRC(PartFactory):
 
     def state(self, file):
         file.fmt('''
-		|	uint8_t traram[1 << BUS_A_WIDTH];
+		|	uint8_t traram[1 << 14];
 		|	unsigned trareg;
+		|	unsigned trace_q1;
+		|	unsigned trace_q3;
+		|	unsigned col;
+		|	unsigned cnt1, cnt2, cnt8;
+		|	unsigned co8;
+		|	bool traddr12;
+		|	bool traddr13;
 		|''')
-
-    def xxsensitive(self):
-        yield "PIN_FIU_CLK.pos()"
-        yield "PIN_LOCAL_CLK.pos()"
-        yield "PIN_Q1not.pos()"
-        yield "PIN_DV_U"
-        yield "PIN_BAD_HINT"
-        yield "PIN_U_PEND"
 
     def private(self):
         enables = (
-            "PIN_CLK.posedge_event()",
-            "PIN_WE.posedge_event()",
-            "PIN_IAE0",
-            "PIN_IAE1",
+            "PIN_CLKQ2",
+            "PIN_CLKQ4",
             "PIN_IBE0",
-            "PIN_IBE1",
             "PIN_ICE0",
             "PIN_ICE1",
             "PIN_IDE",
+            "BUS_MODE",
+            "PIN_RFSH",
+            "PIN_DIR",
+            "PIN_ADIR",
         )
         yield from self.event_or(
             "ia_event",
@@ -74,7 +74,9 @@ class XMTRC(PartFactory):
         )
         yield from self.event_or(
             "ib_event",
-            "BUS_IB",
+            "PIN_COLE",
+            "PIN_ROWE",
+            "BUS_ROW",
             *enables,
         )
         yield from self.event_or(
@@ -89,7 +91,6 @@ class XMTRC(PartFactory):
         )
         yield from self.event_or(
             "ram_event",
-            "BUS_A",
             *enables,
         )
 
@@ -99,17 +100,113 @@ class XMTRC(PartFactory):
         super().doit(file)
 
         file.fmt('''
-		|	if (PIN_CLK.posedge()) {
+		|	unsigned mode;
+		|	BUS_MODE_READ(mode);
+		|	bool refresh = PIN_RFSH=>;
+		|	bool trace_dir = PIN_DIR=>;
+		|	bool tr_dram_dir = mode < 0xc;
+		|
+		|	bool tradr_oe = !(!refresh || mode == 0xa || mode == 0xb);
+		|	bool trace_oe = !((!refresh) ||	(mode < 0x8) ||	(mode == 0xc) || (mode == 0xf));
+		|	bool q2pos = PIN_CLKQ2.posedge();
+		|	bool q2neg = PIN_CLKQ2.negedge();
+		|	bool q4pos = PIN_CLKQ4.posedge();
+		|	bool q4neg = PIN_CLKQ4.negedge();
+		|
+		|	unsigned diag;
+		|	BUS_IA_READ(diag);
+		|	unsigned dcnt;
+		|	BUS_DCNT_READ(dcnt);
+		|
+		|	if (q4pos) {
+		|		unsigned cnt1 = state->cnt1;
+		|		unsigned cnt2 = state->cnt2;
+		|		bool cen =
+		|		    !(
+		|			((!refresh) && (!(cnt2 & 1)) && ((cnt1) & 5) == 5) ||
+		|			(  refresh  && ((dcnt & 0x1d) == 0x05)) ||
+		|			(  refresh  && ((dcnt & 0x1e) == 0x04))
+		|		    );
+		|
 		|		BUS_ID_READ(state->trareg);
+		|		if (!PIN_C8LD=>) {
+		|			BUS_IA_READ(state->cnt8);
+		|		} else if (!cen) {
+		|			state->cnt8 += 1;
+		|		}
+		|		state->cnt8 &= 0xff;
+		|		output.qta = state->cnt8;
+		|
+		|		if (!refresh) {
+		|			if (cnt2 & 1) {
+		|			} else if (cnt1 == 0x0) {
+		|				cnt1 = 0x3;
+		|			} else if (cnt1 == 0x3) {
+		|				cnt1 = 0xc;
+		|			} else if (cnt1 == 0xc) {
+		|				cnt1 = 0xf;
+		|			} else {
+		|				cnt1 = 0x0;
+		|			}
+		|			cnt2 ^= 0x1;
+		|		} else {
+		|			switch (dcnt) {
+		|			case 0x04:
+		|				if (!state->co8)
+		|					cnt1 = state->cnt1 + 1;
+		|				break;
+		|			case 0x06:
+		|				cnt2 = state->cnt2 + 1;
+		|				break;
+		|			case 0x07:
+		|				if (!state->co8)
+		|					cnt2 = state->cnt2 + 1;
+		|				break;
+		|			case 0x0c:
+		|				cnt1 = state->cnt2;
+		|				break;
+		|			case 0x0d:
+		|				cnt2 = state->cnt1;
+		|				break;
+		|			case 0x0e:
+		|				cnt1 = diag >> 4;
+		|				break;
+		|			case 0x0f:
+		|				cnt2 = diag >> 4;
+		|				break;
+		|			default:
+		|				if (dcnt >= 0x10)
+		|					cnt2 = dcnt & 0xf;
+		|				break;
+		|			}
+		|		}
+		|		state->cnt1 = cnt1;
+		|		state->cnt2 = cnt2;
+		|		state->co8 = (state->cnt8 != 0xff);
+		|		output.tradr = cnt1;
+		|		output.ta3 = cnt1 & 1;
 		|	}
 		|
-		|	unsigned trace = 0xff, adr = 0;
+		|	output.ovfo =
+		|	    !(
+		|		((dcnt == 0x05) && (!state->co8)) ||
+		|		(((dcnt & 0x1d) == 0x05) && (!state->co8) && state->cnt2 == 0xf ) ||
+		|		((dcnt == 0x06) &&  state->cnt2 == 0xf ) ||
+		|		(((dcnt & 0x1e) == 0x04) && (!state->co8) && state->cnt1 == 0xf )
+		|	    );
 		|
-		|	if (!PIN_IAE0=> && PIN_IAE1=>) {
+		|	unsigned trace, adr;
+		|	bool needadr = false;
+		|
+		|	if (!trace_oe && trace_dir) {
 		|		BUS_IA_READ(trace);
 		|		next_trigger(ia_event);
-		|	} else if (!PIN_IBE0=> && !PIN_IBE1=>) {
-		|		BUS_IB_READ(trace);
+		|	} else if (!PIN_IBE0=> && !tr_dram_dir) {
+		|		if (!PIN_ROWE=>) {
+		|			BUS_ROW_READ(trace);
+		|		} else if (!PIN_COLE=>) {
+		|			trace = state->col;
+		|		}
 		|		next_trigger(ib_event);
 		|	} else if (!PIN_ICE0=> && !PIN_ICE1=>) {
 		|		BUS_IC_READ(trace);
@@ -118,25 +215,121 @@ class XMTRC(PartFactory):
 		|		trace = state->trareg;
 		|		next_trigger(id_event);
 		|	} else {
-		|		BUS_A_READ(adr);
-		|		trace = state->traram[adr];
-		|		next_trigger(ram_event);
-		|	}
-		|	output.q = trace;
-		|	if (PIN_WE.posedge()) {
-		|		BUS_A_READ(adr);
-		|		state->traram[adr] = trace;
+		|		needadr = true;
 		|	}
 		|
-		|	TRACE(
-		|		<< " clk " << PIN_CLK?
-		|		<< " we " << PIN_WE?
-		|		<< " ia " << PIN_IAE0? << PIN_IAE1? << " " << BUS_IA_TRACE()
-		|		<< " ib " << PIN_IBE0? << PIN_IBE1? << " " << BUS_IB_TRACE()
-		|		<< " ic " << PIN_ICE0? << PIN_ICE1? << " " << BUS_IC_TRACE()
-		|		<< " id " << PIN_IDE? << " " << BUS_ID_TRACE()
-		|		<< " a " << BUS_A_TRACE()
-		|	);
+		|	bool dowrite = false;
+		|	if (mode == 0xb && (q2pos || q4pos))
+		|		dowrite = true;
+		|	if ((mode & 0xc) == 0x4 && q4pos)
+		|		dowrite = true;
+		|
+		|	if (needadr || dowrite) {
+		|		adr = 0;
+		|		if (state->traddr12) adr |= 2;
+		|		if (state->traddr13) adr |= 1;
+		|		unsigned ahi = state->cnt1;
+		|		adr |= ahi << 10;
+		|		if (!tradr_oe) {
+		|			unsigned diag2;
+		|			BUS_IA_READ(diag2);
+		|			adr |= diag2 << 2;
+		|		} else {
+		|			adr |= state->cnt8 << 2;
+		|		}
+		|		if (needadr) {
+		|			trace = state->traram[adr];
+		|			next_trigger(ram_event);
+		|		}
+		|	}
+		|	output.q = trace;
+		|
+		|	if (!trace_oe && !trace_dir) {
+		|		output.z_qdiag = 0;
+		|		output.qdiag = trace;
+		|	} else if (!tradr_oe && !PIN_ADIR=>) {
+		|		output.z_qdiag = 0;
+		|		output.qdiag = state->cnt8;
+		|	} else {
+		|		output.z_qdiag = 1;
+		|	}
+		|
+		|	if (q2neg) {
+		|		state->trace_q1 = trace;
+		|		if (!PIN_ROWE=>) {
+		|			BUS_ROW_READ(output.dr);
+		|		} else if (!PIN_COLE=>) {
+		|			output.dr = state->col;
+		|		} else {
+		|			output.dr = trace;
+		|		}
+		|	}
+		|	if (q2pos) {
+		|		BUS_COL_READ(state->col);
+		|		output.tqb = trace;
+		|		if (dowrite)
+		|			state->traram[adr] = trace;
+		|	}
+		|	if (q4neg) {
+		|		state->trace_q3 = trace;
+		|		if (!PIN_ROWE=>) {
+		|			BUS_ROW_READ(output.dr);
+		|		} else if (!PIN_COLE=>) {
+		|			output.dr = state->col;
+		|		} else {
+		|			output.dr = trace;
+		|		}
+		|	}
+		|	if (q4pos) {
+		|		if (PIN_TSCEN=>) {
+		|			output.tstr = state->trace_q1 << 8;
+		|			output.tstr |= state->trace_q3;
+		|		}
+		|		if (dowrite)
+		|			state->traram[adr] = trace;
+		|	}
+		|
+		|	if (q2pos) {
+		|		switch (mode) {
+		|		case 0x2: state->traddr12 = true; break;
+		|		case 0x3: state->traddr12 = true; break;
+		|		case 0x6: state->traddr12 = true; break;
+		|		case 0x7: state->traddr12 = true; break;
+		|		case 0x8: state->traddr12 = true; break;
+		|		case 0xd: state->traddr12 = true; break;
+		|		default: state->traddr12 = false; break;
+		|		};
+		|	}
+		|	if (q4pos) {
+		|		switch (mode) {
+		|		case 0x2: state->traddr12 = true; break;
+		|		case 0x3: state->traddr12 = true; break;
+		|		case 0x6: state->traddr12 = true; break;
+		|		case 0x7: state->traddr12 = true; break;
+		|		default: state->traddr12 = false; break;
+		|		};
+		|	}
+		|	if (q2neg) {
+		|		switch (mode) {
+		|		case 0x2: state->traddr13 = true; break;
+		|		case 0x6: state->traddr13 = true; break;
+		|		case 0x1: state->traddr13 = true; break;
+		|		case 0x5: state->traddr13 = true; break;
+		|		case 0x8: state->traddr13 = true; break;
+		|		default: state->traddr13 = false; break;
+		|		};
+		|	}
+		|	if (q4neg) {
+		|		switch (mode) {
+		|		case 0x2: state->traddr13 = true; break;
+		|		case 0x6: state->traddr13 = true; break;
+		|		case 0x1: state->traddr13 = true; break;
+		|		case 0x5: state->traddr13 = true; break;
+		|		default: state->traddr13 = false; break;
+		|		};
+		|	}
+		|
+		|
 		|''')
 
 def register(part_lib):
--- Schematics/Symbols/PySymbols/xmtrc.py
+++ Schematics/Symbols/PySymbols/xmtrc.py
@@ -13,15 +13,12 @@ class XMTRC(FChip):
     def __init__(self):
         super().__init__()
 
-        self.sig_left(ChipSig("-->+", "CLK"))
+        self.sig_left(ChipSig("-->+", "CLKQ2"))
+        self.sig_left(ChipSig("-->+", "CLKQ4"))
 
         self.sig_left(ChipSig("-->+", "IA", 0, 7))
-        self.sig_left(ChipSig("-->o", "IAE0"))
-        self.sig_left(ChipSig("-->+", "IAE1"))
 
-        self.sig_left(ChipSig("-->+", "IB", 0, 7))
         self.sig_left(ChipSig("-->o", "IBE0"))
-        self.sig_left(ChipSig("-->o", "IBE1"))
 
         self.sig_left(ChipSig("-->+", "IC", 0, 7))
         self.sig_left(ChipSig("-->o", "ICE0"))
@@ -30,10 +27,28 @@ class XMTRC(FChip):
         self.sig_left(ChipSig("-->+", "ID", 0, 7))
         self.sig_left(ChipSig("-->+", "IDE"))
 
-        self.sig_left(ChipSig("-->+", "A", 0, 13))
-        self.sig_left(ChipSig("-->+", "WE"))
+        self.sig_left(ChipSig("-->+", "MODE", 0, 3))
+        self.sig_left(ChipSig("-->+", "DIR"))
+        self.sig_left(ChipSig("-->+", "ADIR"))
+        self.sig_left(ChipSig("-->+", "RFSH"))
+        self.sig_left(ChipSig("-->+", "C8LD"))
+
+        self.sig_left(ChipSig("-->+", "TSCEN"))
+        self.sig_left(ChipSig("-->+", "ROWE"))
+        self.sig_left(ChipSig("-->+", "ROW", 0, 7))
+        self.sig_left(ChipSig("-->+", "COLE"))
+        self.sig_left(ChipSig("-->+", "COL", 0, 7))
+        self.sig_left(ChipSig("-->+", "DCNT", 0, 4))
 
         self.sig_right(ChipSig("+-->", "Q", 0, 7))
+        self.sig_right(ChipSig("+===", "QDIAG", 0, 7))
+        self.sig_right(ChipSig("+-->", "QTA", 4, 11))
+        self.sig_right(ChipSig("+-->", "TQB", 0, 7))
+        self.sig_right(ChipSig("+-->", "DR", 3, 10))
+        self.sig_right(ChipSig("+-->", "TSTR", 0, 15))
+        self.sig_right(ChipSig("+-->", "TA3"))
+        self.sig_right(ChipSig("+-->", "OVFO"))
+        self.sig_right(ChipSig("+-->", "TRADR", 0, 3))
 
         self.finish()
 
