9968296743059b006756a9b01acf442191bc36e0
Poul-Henning Kamp <phk@FreeBSD.org>
2024-06-15 05:53:22 +0000

    Add note.

--- Emulator/Components/tables.c
+++ Emulator/Components/tables.c
@@ -2,6 +2,10 @@
 
 #include "Components/tables.h"
 
+/*
+ * The order of the four S signals are reversed relative to the datasheet
+ * to match the same the R1000 schematic symbol.
+ */
 const uint8_t lut181[16384] = {
 	#include "F181_tbl.h"
 };
