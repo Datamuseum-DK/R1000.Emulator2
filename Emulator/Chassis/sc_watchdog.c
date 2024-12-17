fe5e4fb64bb1c4c5400de87dc5452413fe6005ac
Poul-Henning Kamp <phk@FreeBSD.org>
2024-12-17 06:56:28 +0000

    Squeeze FIDO reports to fit 80 char line

--- Emulator/Chassis/sc_watchdog.c
+++ Emulator/Chassis/sc_watchdog.c
@@ -70,13 +70,14 @@ fido(void *priv)
 		if (el > 0) {
 			d = 1e-9 * (t1.tv_nsec - sc_t0.tv_nsec);
 			d += (t1.tv_sec - sc_t0.tv_sec);
-			printf("FIDO: r %.3f s %.6f ds %.6f / %.3f",
+			printf("FIDO: r %.1f s %.3f ds %.3f / %.1f",
 			    d, e, e - el, d / e);
 			if (e - el > 0)
-				printf("  d/' %.3f", (d - dl) / (e - el));
+				printf("  d/' %.1f", (d - dl) / (e - el));
 			else
-				printf("  d/' %.3f", 0.0 );
-			printf(" da %.0f", (double)(this_act - last_act) / dt);
+				printf("  d/' %.1f", 0.0 );
+			printf(" kda %.1f",
+			    (double)(.001 * (this_act - last_act)) / dt);
 			printf(" dm %.1f", (double)(this_instr - last_instr) / dt);
 			printf(" de %.1f", (double)(this_exec - last_exec) / dt);
 			printf("\n");
