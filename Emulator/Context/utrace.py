8913c1877879a448f2f05d195e625174d5428fbc
Poul-Henning Kamp <phk@FreeBSD.org>
2025-12-23 14:54:49 +0000

    Update utrace script

--- Emulator/Context/utrace.py
+++ Emulator/Context/utrace.py
@@ -9,45 +9,17 @@ import context
 
 def main():
     ''' ... '''
-    tram = [0] * 2048
-    tcnt = 0
-    branch = set()
+    tram = [0] * 2049
     for ctx in context.contexts():
-        if "ioc_55" not in ctx.ident:
+        if "IOC_TRAM" not in ctx.ident:
             continue
-        if ctx.ident == "IOC.ioc_55.TACNT":
-            i = struct.unpack("I", ctx.body[:4])
-            tcnt = i[0]
-            branch.add("megacomp2")
-        elif ctx.ident == "IOC.ioc_55.TRAM":
-            tram = struct.unpack("2048H", ctx.body)
-            branch.add("megacomp2")
-        elif ctx.ident == "IOC.ioc_55.TACNT0":
-            i = struct.unpack("I", ctx.body[:4])
-            tcnt |= i[0] << 8
-            branch.add("main")
-        elif ctx.ident == "IOC.ioc_55.TACNT1":
-            i = struct.unpack("I", ctx.body[:4])
-            tcnt |= i[0] << 4
-            branch.add("main")
-        elif ctx.ident == "IOC.ioc_55.TACNT2":
-            i = struct.unpack("I", ctx.body[:4])
-            tcnt |= i[0]
-            branch.add("main")
-        elif ctx.ident == "IOC.ioc_55.TRAM0":
-            ram = struct.unpack("2048H", ctx.body)
-            branch.add("main")
-            for i in range(2048):
-                tram[i] |= ram[i] << 8
-        elif ctx.ident == "IOC.ioc_55.TRAM1":
-            ram = struct.unpack("2048H", ctx.body)
-            branch.add("main")
-            for i in range(2048):
-                tram[i] |= ram[i]
+        tram = list(struct.unpack("2049H", ctx.body[:2049*2]))
+        break
+    tcnt = tram.pop(-1)
     if sum(tram) == 0:
-        print("NO UTRACE", branch)
+        print("NO UTRACE")
         return
-    print("UTRACE:", hex(tcnt), branch)
+    print("UTRACE:", hex(tcnt))
     tcnt &= 0x3ff
     trace = list(tram[tcnt:] + tram[:tcnt])
     while trace[0] == 0:
